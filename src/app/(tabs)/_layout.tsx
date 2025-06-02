9b1b9b68ac4bd64c54e2ffb2175e8f1fda35f956
Lucas Silva <lucassdeoliveira@outlook.com.br>
2025-06-01 23:04:42 -0300

    refactor: remove _layout.tsx from tabs home

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,9 +1,44 @@
-import { Stack, Tabs } from "expo-router";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Layout() {
-    return (
-        <Tabs>
-            <Stack.Screen name="home/index" options={{headerShown: false}}/>
-        </Tabs>
-    )
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#007aff",
+      }}
+    >
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: "Início",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="criarTarefa/index"
+        options={{
+          title: "Adicionar Tarefa",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle-outline" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="perfil/index"
+        options={{
+          title: "Seu Perfil",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+
+    
+  );
 }
