8bfafa1d8689627e46224e11e31c9aa465f64229
Lucas Silva <lucassdeoliveira@outlook.com.br>
2025-06-01 23:09:56 -0300

    feat: cria pasta para adicionar a perfil

---
+++ src/app/(tabs)/perfil/index.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+
+export default function PerfilScreen() { 
+    return (
+        <View>
+
+            <Text>Olá</Text>
+        </View>
+    )
+}
