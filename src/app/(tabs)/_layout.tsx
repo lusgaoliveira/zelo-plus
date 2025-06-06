42e17cf618c6bd33f1cfc2232c46605a20581082
Lucas Silva <lucassdeoliveira@outlook.com.br>
2025-06-05 23:30:28 -0300

    refactor: edita o layout de tabs

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,41 +3,71 @@ import { Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout() {
   const { dados } = useLocalSearchParams();
+  const usuario = dados ? JSON.parse(dados as string) : null;
+  const tipoUsuario = usuario?.tipoUsuario;
 
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
-        tabBarActiveTintColor: "#007aff",
+        tabBarActiveTintColor: "#FFB956",
+        tabBarInactiveTintColor: "#000",
+        tabBarStyle: {
+          backgroundColor: "#FFFAEC",
+          borderTopWidth: 0,
+          height: 100,
+          paddingBottom: 20,
+          paddingTop: 10,
+          elevation: 0,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
       }}
     >
       <Tabs.Screen
         name="home/index"
         options={{
           title: "Início",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home-outline" size={size} color={color} />
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home-outline" size={30} color={color} />
           ),
         }}
         initialParams={{ dados }}
       />
+
       <Tabs.Screen
         name="criarTarefa/index"
         options={{
           title: "Adicionar Tarefa",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="add-circle-outline" size={size} color={color} />
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="add-circle-outline" size={30} color={color} />
           ),
         }}
         initialParams={{ dados }}
       />
+
       <Tabs.Screen
         name="perfil/index"
         options={{
           title: "Seu Perfil",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person-outline" size={size} color={color} />
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person-outline" size={30} color={color} />
           ),
+        }}
+        initialParams={{ dados }}
+      />
+
+      <Tabs.Screen
+        name="relatorio/index"
+        options={{
+          title: "Relatório",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="bar-chart-outline" size={30} color={color} />
+          ),
+          // Oculta a aba para IDOSO
+          tabBarButton: tipoUsuario === "IDOSO" ? () => null : undefined,
         }}
         initialParams={{ dados }}
       />
