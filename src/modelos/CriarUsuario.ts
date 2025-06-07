cba992e6ef002517fd081c8da4beb7a4d3988dbb
Lucas Silva <lucassdeoliveira@outlook.com.br>
2025-06-07 15:52:50 -0300

    refactor: remove foto perfil do modelo

--- src/modelos/CriarUsuario.ts
+++ src/modelos/CriarUsuario.ts
@@ -2,7 +2,6 @@ export interface CriarUsuario {
   nomeUsuario: string;
   senha: string;
   email: string;
-  fotoPerfil: string;
   nome: string;
   tipoUsuario: string;
   dataNascimento: string; 
