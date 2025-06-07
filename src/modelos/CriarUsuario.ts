3cca71bb5bc09385b6642ff4eb0eb9671db2de99
Lucas Silva <lucassdeoliveira@outlook.com.br>
2025-06-06 23:30:18 -0300

    feat: adiciona tipo usuario

--- src/modelos/CriarUsuario.ts
+++ src/modelos/CriarUsuario.ts
@@ -2,8 +2,9 @@ export interface CriarUsuario {
   nomeUsuario: string;
   senha: string;
   email: string;
-  fotoPerfilUrl: string;
+  fotoPerfil: string;
   nome: string;
+  tipoUsuario: string;
   dataNascimento: string; 
   codigoVinculo: string | null;
 }
